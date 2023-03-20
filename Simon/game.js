6ed290d867cc9c2bede2d6ac196ab92eed818df1
BCSBruh <jeromelarson26@gmail.com>
2023-03-20 13:42:29 -0400

    Finished Simon Says

--- Simon/game.js
+++ Simon/game.js
@@ -3,13 +3,17 @@ var gamePattern = [];
 var userClickedPattern = [];
 var gameOver = true;
 var level = 0;
+var counter = 0;
 
 function nextSequence() {
-  var randomChosenColor = buttonColors[Math.floor(Math.random() * 4)];
-  gamePattern.push(randomChosenColor);
-  level++;
-  $("h1").text("Level " + level);
-  animateSequence();
+  if (!gameOver) {
+    var randomChosenColor = buttonColors[Math.floor(Math.random() * 4)];
+    gamePattern.push(randomChosenColor);
+    level++;
+    $("h1").text("Level " + level);
+    animateSequence();
+    counter = 0;
+  }
 }
 
 async function animateSequence() {
@@ -26,13 +30,17 @@ $(".btn").on("click", function (event) {
     pressButton(userChosenColor);
     playSound(userChosenColor);
     userClickedPattern.push(userChosenColor);
-    console.log(userClickedPattern);
+    var isValid = checkAnswer(userClickedPattern, gamePattern, counter);
+    if (isValid === false) {
+      gameIsOver();
+      console.log(isValid);
+    }
+    counter++;
   }
 
   if (userClickedPattern.length === gamePattern.length) {
     nextSequence();
     userClickedPattern = [];
-    console.log(userClickedPattern);
   }
 });
 
@@ -48,12 +56,34 @@ function pressButton(name) {
 
 $(document).on("keydown", function () {
   if (gameOver) {
-    nextSequence();
     gameOver = false;
+    nextSequence();
   }
 });
 
-
 function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function checkAnswer(userArr, gameArr, counter) {
+  if (userArr[counter] === gameArr[counter]) {
+    return true;
+  } else {
+    return false;
   }
+}
+
+async function gameIsOver() {
+  gameOver = true;
+  gamePattern = [];
+  userClickedPattern = [];
+  playSound("wrong");
+
+  $("#level-title").text("Game Over. Press any key to restart");
+
+  $("body").addClass("game-over");
+  await sleep(200);
+  $("body").removeClass("game-over");
+  level = 0;
+  counter = 0;
+}
