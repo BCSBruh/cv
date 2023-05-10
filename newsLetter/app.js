3f214914d12198d6cb963bfbc597b21f4cacf654
BCSBruh <87160862+BCSBruh@users.noreply.github.com>
2023-05-10 02:57:40 -0400

    bruh

--- newsLetter/app.js
+++ newsLetter/app.js
@@ -51,6 +51,6 @@ app.post("/", function (req, res) {
   request.end();
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Listening on port 3000");
 });
