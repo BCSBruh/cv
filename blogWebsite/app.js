19fc47976a88b3e363197801dcc5f272fcaf4c91
BCSBruh <jeromelarson26@gmail.com>
2023-05-16 21:23:19 -0400

    Started adding the DB to the blog

--- blogWebsite/app.js
+++ blogWebsite/app.js
@@ -1,38 +1,60 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const path = require('path');
+const path = require("path");
 const lodash = require("lodash");
-const trunc = require(__dirname + '/truncate.js');
-const mongoose = require("mongoose")
+const trunc = require(__dirname + "/truncate.js");
+const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://BCSBruh:Dogman26@blog.yzwaere.mongodb.net/blogDB")
+mongoose.connect(
+  "mongodb+srv://BCSBruh:Dogman26@blog.yzwaere.mongodb.net/blogDB"
+);
 
 const app = express();
 
 let posts = [];
 let truncPosts = [];
 
+const blogSchema = new mongoose.Schema({
+  title: String,
+  body: String,
+  type: String,
+});
+
+const userSchema = new mongoose.Schema({
+  userName: String,
+  userPassword: String,
+  userType: Number,
+});
+
+const BlogPost = mongoose.model("BlogPost", blogSchema);
+const User = mongoose.model("User", userSchema);
+
 app.set("view engine", "ejs");
-app.set('views', path.join(__dirname, 'views'));
+app.set("views", path.join(__dirname, "views"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const homeStartingContent = "Welcome to my blog website. Below you will find some of the blogs I have written during my journey through college and learning Web Development. I am currently in college and one semester away from graduating.";
-const aboutContent = "This website was made by myself and with the help of Dr. Angela Yu.";
+const homeStartingContent =
+  "Welcome to my blog website. Below you will find some of the blogs I have written during my journey through college and learning Web Development. I am currently in college and one semester away from graduating.";
+const aboutContent =
+  "This website was made by myself and with the help of Dr. Angela Yu.";
 const contactContent = "Contact me at the following locations:";
 
 app.get("/", function (req, res) {
-  res.render("home", {homeStartingContent: homeStartingContent, truncPosts: truncPosts});
+  res.render("home", {
+    homeStartingContent: homeStartingContent,
+    truncPosts: truncPosts,
+  });
 });
 
 app.get("/about", function (req, res) {
-  res.render("about", {aboutContent: aboutContent});
+  res.render("about", { aboutContent: aboutContent });
 });
 
 app.get("/contact", function (req, res) {
-  res.render("contact", {contactContent: contactContent});
+  res.render("contact", { contactContent: contactContent });
 });
 
 app.get("/compose", function (req, res) {
@@ -40,29 +62,47 @@ app.get("/compose", function (req, res) {
 });
 
 app.get("/posts/:post", function (req, res) {
-  posts.forEach(function(i) {
+  posts.forEach(function (i) {
     if (lodash.lowerCase(req.params.post) === lodash.lowerCase(i.title)) {
-      res.render("post", {postTitle: i.title, postBody: i.body});
+      res.render("post", { postTitle: i.title, postBody: i.body });
     }
-  })
-})
+  });
+});
 
 app.post("/compose", function (req, res) {
-  let post = {
-    title: req.body.composedTitle,
-    body: req.body.composedBody
-  }
+  // let post = {
+  //   title: req.body.composedTitle,
+  //   body: req.body.composedBody,
+  // };
 
-  let truncPost = {
-    title: req.body.composedTitle,
-    body: trunc.truncate(req.body.composedBody),
-    lowerTitle: "/posts/" + lodash.kebabCase(req.body.composedTitle)
-  }
+  // let truncPost = {
+  //   title: req.body.composedTitle,
+  //   body: trunc.truncate(req.body.composedBody),
+  //   lowerTitle: "/posts/" + lodash.kebabCase(req.body.composedTitle),
+  // };
 
-  posts.push(post);
-  truncPosts.push(truncPost);
+  let postTitle = req.body.composedTitle;
+  let postBody = req.body.composedBody;
 
-  res.redirect("/");
+  // Save the post to the database
+  BlogPost.findOne({ title: postTitle }).then((post) => {
+    if (post === null) {
+      let post = new BlogPost({
+        title: postTitle,
+        body: postBody,
+        type: "blogPost",
+      });
+
+      post.save();
+      res.redirect("/");
+    } else {
+      // Create Error message
+      res.redirect("/");
+    }
+  });
+
+  // posts.push(post);
+  // truncPosts.push(truncPost);
 });
 
 app.listen(3000, function () {
